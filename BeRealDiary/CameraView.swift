66407b6ba8ab59595fa7113a2478d125b6ac0b3d
ankasamanyan <anikakasamanyan@gmail.com>
2025-03-06 17:29:52 +0100

    feat: initial camera view created

--- BeRealDiary/CameraView.swift
+++ BeRealDiary/CameraView.swift
@@ -1,18 +1,50 @@
-//
-//  SwiftUIView.swift
-//  BeRealDiary
-//
-//  Created by Anait Kasamanian on 06.03.25.
-//
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct CameraView: View {
+    @Binding var imageData: Data?
+    @Binding var showCamera: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack {
+                // the main screen
+                cameraPreview
+                HStack {
+                    //the future gallery icon
+                    Image(systemName: "square.fill")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 32))
+                        .overlay(Rectangle().stroke(Color.white, lineWidth: 2))
+                        .padding(.trailing)
+                    // the Camera button
+                    ZStack {
+                        Circle()
+                            .frame(width: 90, height: 90)
+                            .foregroundColor(.indigo)
+                            .padding()
+                        Button(action: {showCamera.toggle()}) {
+                            Image(systemName: "camera.fill")
+                                .font(.system(size: 32))
+
+                        }
+                        .foregroundColor(.white)
+                    }
+                    .ignoresSafeArea(edges: .all)
+                    .padding(.trailing, 30)
+                }
+                .padding(.trailing, 20)
+            }
+        }
+    }
+
+    private var cameraPreview: some View {
+        Color.indigo
     }
 }
 
 #Preview {
-    SwiftUIView()
+    CameraView(imageData: .constant(nil), showCamera: .constant(true))
 }
--- BeRealDiary/ContentView.swift
+++ BeRealDiary/ContentView.swift
@@ -1,24 +1,43 @@
-//
-//  ContentView.swift
-//  BeRealDiary
-//
-//  Created by Anait Kasamanian on 06.03.25.
-//
-
 import SwiftUI
 
-struct ContentView: View {
+struct BeRealDiary: View {
+
+    @State private var imageData: Data?
+    @State private var showCamera: Bool = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let imageData, let uiImage = UIImage(data: imageData) {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+            } else {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .foregroundStyle(.gray)
+            }
+            ZStack {
+                Button("Take a photo") {
+                    self.showCamera.toggle()
+                }
+                .buttonStyle(.borderedProminent)
+                .padding()
+            }
         }
         .padding()
+        .fullScreenCamera(isPresented: $showCamera, imageData: $imageData)
+    }
+}
+
+extension View {
+    func fullScreenCamera(isPresented: Binding<Bool>, imageData: Binding<Data?>) -> some View {
+        self.fullScreenCover(isPresented: isPresented, content: {
+            CameraView(imageData: imageData, showCamera: isPresented)
+        })
     }
 }
 
 #Preview {
-    ContentView()
+    BeRealDiary()
 }
