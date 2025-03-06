265c39318099f3b4d651577438c89ee65d8ee9fd
Anait Kasamanian <anait.kasamanian@otto.de>
2025-03-06 12:51:02 +0100

    Initial Commit

---
+++ BeRealDiary/BeRealDiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealDiaryApp.swift
+//  BeRealDiary
+//
+//  Created by Anait Kasamanian on 06.03.25.
+//
+
+import SwiftUI
+
+@main
+struct BeRealDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeRealDiaryTests/BeRealDiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealDiaryTests.swift
+//  BeRealDiaryTests
+//
+//  Created by Anait Kasamanian on 06.03.25.
+//
+
+import Testing
+@testable import BeRealDiary
+
+struct BeRealDiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
