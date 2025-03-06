fc0fbacc67e78c6b46e73a8638176f7a65d75b38
ankasamanyan <anikakasamanyan@gmail.com>
2025-03-06 17:03:33 +0100

    first commit

---
+++ BeRealDiary/CameraView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  BeRealDiary
+//
+//  Created by Anait Kasamanian on 06.03.25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
